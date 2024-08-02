b94507c7376335bf9d5d5eea7136b4d37f0de393
Nissim Meyer <nissim2305@gmail.com>
2024-08-02 11:13:15 +0300

    Updated main python file

--- pass_app.py
+++ pass_app.py
@@ -2,9 +2,6 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Master password
-master_password = "masterpass"
-
 # Sample list of passwords
 passwords = [
     {"id": 1, "username": "example.com", "password": "password123"},
@@ -13,24 +10,18 @@ passwords = [
 ]
 
 # Route to get all passwords
-@app.route('/api/passwords/', methods=['GET'])
+@app.route('/passwords/', methods=['GET'])
 def get_passwords():
-    if "pass" in request.args and request.args["pass"] == master_password:
-        return jsonify(passwords)
-    else:
-        return jsonify({"error": "Master password required"}), 401
-
+    return jsonify(passwords)
+    
 # Route to get a password by ID
-@app.route('/api/passwords/<int:password_id>/', methods=['GET'])
+@app.route('/passwords/<int:password_id>/', methods=['GET'])
 def get_password(password_id):
-    if "pass" in request.args and request.args["pass"] == master_password:
-        password = next((password for password in passwords if password["id"] == password_id), None)
-        if password:
-            return jsonify(password)
-        else:
-            return jsonify({"error": "Password not found"}), 404
+    password = next((password for password in passwords if password["id"] == password_id), None)
+    if password:
+        return jsonify(password)
     else:
-        return jsonify({"error": "Master password required"}), 401
+        return jsonify({"error": "Password not found"}), 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5005, debug=True)
