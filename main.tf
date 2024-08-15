b322731648e65f2cab25a362b58de0976a4fa989
Nissim Meyer <nissim2305@gmail.com>
2024-08-15 14:25:09 +0300

    changed on 15.8

--- main.tf
+++ main.tf
@@ -1,33 +1,35 @@
+# Configure the Kubernetes provider with the path to your kubeconfig file
 provider "kubernetes" {
   config_path = "~/.kube/config"
 }
 
-resource "kubernetes_deployment" "myapp-pass" {
+# Define a Kubernetes deployment resource
+resource "kubernetes_deployment" "passapp" {
   metadata {
-    name = "myapp-pass"
+    name = "passapp"                   # The name of the deployment
     labels = {
-      app = "myapp-pass"
+      app = "passapp"                  # Labels for the deployment
     }
   }
   spec {
-    replicas = 2
+    replicas = 2                       # Number of replicas (Pods) to create
     selector {
       match_labels = {
-        app = "myapp-pass"
+        app = "passapp"                # Match the labels to identify Pods managed by this deployment
       }
     }
     template {
       metadata {
         labels = {
-          app = "myapp-pass"
+          app = "passapp"              # Labels for the Pod template
         }
       }
       spec {
         container {
-          image = "nissim2305/myapp-pass"
-          name  = "myapp-pass"
+          image = "nissim2305/pass_app:latest"  # Docker image for the container
+          name  = "passapp"            # Name of the container
           port {
-            container_port = 5000
+            container_port = 5000      # Container port to expose
           }
         }
       }
@@ -35,18 +37,20 @@ resource "kubernetes_deployment" "myapp-pass" {
   }
 }
 
-resource "kubernetes_service" "myapp-pass" {
+# Define a Kubernetes service resource
+resource "kubernetes_service" "passapp" {
   metadata {
-    name = "myapp-pass-service"
+    name = "passapp-service"           # The name of the service
   }
   spec {
     selector = {
-      app = "myapp-pass"
+      app = "passapp"                  # Selects Pods with the label 'app=passapp'
     }
     port {
-      port        = 80
-      target_port = 5000
+      port        = 80                 # The port that the service will expose
+      target_port = 5001               # The target port on the Pods
     }
-    type = "NodePort"
+    type = "NodePort"                  # Type of service (NodePort)
   }
 }
+
